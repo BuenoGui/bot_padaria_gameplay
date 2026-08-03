a21d334dc168133540a587722cf5a9d938099fe7
guilherme <buenoguilherme2004@gmail.com>
2026-08-03 16:47:11 -0300

    indo pra faculdade

--- src/services/tests/loja.ts
+++ src/services/tests/loja.ts
@@ -9,13 +9,27 @@ async function comprar_gas(player: Player) {
         "SELECT * FROM padarias WHERE id_player = $1",
         [id_player]
     )
+    // Dados da padaria do Player
     const dados_padaria_player = todos_dados_padaria_player.rows[0]
     let gas_atual_player = Number(dados_padaria_player.gas_atual)
     let gas_total_player = Number(dados_padaria_player.gas_max)
 
-    if (gas_atual_player === gas_total_player) {
-        console.log("Player com gás cheio")
-        return
+    // Dados do player
+    const dinheiro_player = player.dinheiro;
+
+    if (dinheiro_player >= 30) {
+        if (gas_atual_player === gas_total_player) {
+            console.log("Player com gás cheio")
+
+            // REMOVER ANTES DE IR PRO SERVICE
+            await pool.end()
+            return
+        }
+    } else {
+
+        // REMOVER ANTES DE IR PRO SERVICE
+        await pool.end()
+        return console.log(player.id_player," Não tem dinheiro para comprar gás \nDinheiro atual:", player.dinheiro)
     }
     
     let gas_novo_player = gas_atual_player + 10;
@@ -25,12 +39,20 @@ async function comprar_gas(player: Player) {
             "UPDATE padarias SET gas_atual = $1 WHERE id_player = $2",
             [gas_novo_player, id_player]
         )
+
+        console.log(player.id_player,"Comprou gás")
+
     } else {
             await pool.query(
             "UPDATE padarias SET gas_atual = $1 WHERE id_player = $2",
             [gas_total_player, id_player]
         )
+
+        console.log(player.id_player,"Comprou gás")
     }
+
+    // REMOVER ANTES DE IR PRO SERVICE
+    await pool.end()
 
     return
 }
@@ -58,4 +80,4 @@ async function comprar_gas_total(player: Player) {
     await pool.end()
 }
 
-await comprar_gas_total(await sortearPlayer());
+await comprar_gas(await sortearPlayer());
